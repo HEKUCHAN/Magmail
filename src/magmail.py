9823b9dfb363b0f1a3f112802d0dcb2876b32272
HEKUCHAN <heitorhirose@gmail.com>
2023-02-06 01:39:55 +0900

    plain/text、plain/htmlを選択可能にする #3

--- src/magmail.py
+++ src/magmail.py
@@ -4,35 +4,54 @@ import mailbox
 import numpy as np
 import pandas as pd
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 
 from .mail import Mail
 
 
 class Magmail:
-    def __init__(self, mbox_path: str, auto_clean: bool = True):
+    def __init__(
+        self,
+        mbox_path: str,
+        auto_clean: bool = True,
+        content_type: Optional[str] = None,
+    ):
         self.mbox_path: Path = Path(mbox_path)
+        self.auto_clean = auto_clean
+        self.content_type = content_type
         if not os.path.exists(self.mbox_path):
             raise FileNotFoundError()
 
         self.is_dir: bool = os.path.isdir(self.mbox_path)
         self.emails: List[Mail] = []
 
-        self._parse(auto_clean=auto_clean)
+        self._parse()
 
     def __len__(self) -> int:
         return len(self.emails)
 
-    def _parse(self, auto_clean: bool = False) -> None:
+    def _parse(self) -> None:
         if not self.is_dir:
             mail_box = mailbox.mbox(self.mbox_path)
             for message in mail_box:
-                self.emails.append(Mail(message, auto_clean=auto_clean))
+                self.emails.append(
+                    Mail(
+                        message,
+                        auto_clean=self.auto_clean,
+                        content_type=self.content_type,
+                    )
+                )
         else:
             for file in os.listdir(self.mbox_path):
                 mail_box = mailbox.mbox(self.mbox_path / file)
                 for message in mail_box:
-                    self.emails.append(Mail(message, auto_clean=auto_clean))
+                    self.emails.append(
+                        Mail(
+                            message,
+                            auto_clean=self.auto_clean,
+                            content_type=self.content_type,
+                        )
+                    )
 
     def total(self) -> int:
         return self.__len__()
