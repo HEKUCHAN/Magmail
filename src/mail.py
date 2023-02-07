2923a394f59eb970240eea49053b6bc88ff3f6f1
HEKUTA <Heitorhirose@gmail.com>
2023-02-07 15:12:52 +0900

    fix の代入し忘れを修正

--- src/mail.py
+++ src/mail.py
@@ -174,6 +174,7 @@ class Mail:
                     self.__cannot_decode_header(name, decoded_header, detected_charset)
                     continue
             elif isinstance(byte, str):
+                self.detected_header_charset = detected_charset
                 header += byte
 
         return header
