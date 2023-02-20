893e87ca4d66b7fe53fa94363203f518dd170a3a
HEKUCHAN <heitorhirose@gmail.com>
2023-02-21 00:14:01 +0900

    #70 _Headerクラスをunpack対応させる

--- src/magmail/mail/header.py
+++ src/magmail/mail/header.py
@@ -25,6 +25,9 @@ class _Header:
 
         self.decode()
 
+    def __iter__(self):
+        return iter([self.field, self.body])
+
     def decode(self):
         body_parts = []
         for byte, encoding in decode_header(self.body):
@@ -36,12 +39,11 @@ class _Header:
             elif isinstance(byte, str):
                 body_parts.append(byte)
 
-        body = "".join(body_parts)
+        self.body = "".join(body_parts)
 
         if self.auto_clean:
-            body = self.clean_header_value(body)
+            self.body = self.clean_header_value(self.body)
 
-        self.body = body
     @overload
     def clean_header_value(self, header_value: None) -> None:
         ...
@@ -50,17 +52,12 @@ class _Header:
     def clean_header_value(self, header_value: str) -> str:
         ...
 
-    @overload
-    def clean_header_value(self, header_value: List[str]) -> List[str]:
-        ...
-
     def clean_header_value(
         self,
         header_values: Union[Optional[str], List[str]]
     ):
         def clean(value: str) -> str:
             value = NEW_LINE_REGEX.sub('', value)
-            print(value)
             value = value.strip()
             value = URL_REGEX.sub(" ", value)
             value = SPACES_REGEX.sub(" ", value)
@@ -70,11 +67,6 @@ class _Header:
 
             return value
 
-        print(clean('i=1; a=rsa-sha256; c=relaxed/relaxed; d=google.com; s=arc-20160816;\r\n        h=message-id:mime-version:subject:to:reply-to:from:date\r\n         :dkim-signature;\r\n        bh=Ei3/HQJPcOuLp+Z9XRWD68WQoxShRWCww5c7scEPB7U=;\r\n        b=aVLbrnnECPJ3LynJPLdUtBPNVRZG1D4p7nG1RmkwFk51sLmaPRzzFFmS6vrbBmmwIX\r\n         ZAYqGdoAcn0N5iBLSBPOmOn4r0r8Ci34R8xulursS7BffUIVXZ+m011NxvqKwsXh/0Ab\r\n         ve0LGT7GU3TEP/7upNordNlKS7O29ajbMaLMpm4BDyeSuZ/9VI5zfgd2VrbIgaQOJ1r8\r\n         Ge/4wb278bEk5ujfmDhrYqnW7SHdDRq7pm3kqJdi2SIB2EIXGPACem6kiEbeMTOTic3C\r\n         56dnVuBnifpDlWaXLfuwQdLMA9Mc8bqT+guO6yETC+0vhjw7O4mNi4QhbkJU24gTYd8I\r\n         X0+g=='))
-
         if header_values is not None:
-            if isinstance(header_values, list):
-                return [clean(value) for value in header_values]
-            else:
-                return clean(header_values)
+            return clean(header_values)
         return header_values
