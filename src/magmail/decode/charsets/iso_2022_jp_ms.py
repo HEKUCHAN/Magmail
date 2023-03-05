7ce43991364e34b374ec059b3e9858c168ea7c47
HEKUTA <Heitorhirose@gmail.com>
2023-03-06 04:18:39 +0900

    Add iso_2022_jp alias names

--- src/magmail/decode/charsets/iso_2022_jp_ms.py
+++ src/magmail/decode/charsets/iso_2022_jp_ms.py
@@ -1,3 +1,21 @@
+from codecs import CodecInfo
+
+
 def search_iso_2022_jp_ms(name):
-    if name == 'iso_2022_jp':
-        pass
+    alias_name = [
+        'iso_2022_jp_ms',
+        'iso-2022-jp-ms',
+        'iso2022_jp_ms',
+        'iso2022-jp-ms',
+        'iso2022jp_ms',
+        'iso2022jp-ms',
+    ]
+
+    if name not in alias_name:
+        return None
+
+    return CodecInfo(
+        name='iso2022_jp_ms',
+        encode=None,
+        decode=None
+    )
