397dba942bc4abefcfa3be930b466264df00b88f
HEKUTA <Heitorhirose@gmail.com>
2023-03-05 15:39:38 +0900

    python 3.8で動かないため対応した

--- src/magmail/static.py
+++ src/magmail/static.py
@@ -7,41 +7,41 @@ DEFAULT_AUTO_CLEAN = True
 
 
 # REGEX
-ADDRESS_HEADER_REGEX: Pattern[str] = re.compile(
+ADDRESS_HEADER_REGEX: Pattern = re.compile(
     r"[^, ].+?<[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}>|[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
 )
 
-HEADER_NAME_REGEX: Pattern[str] = re.compile(r".*?[^\s](?=<|\s+<)")
+HEADER_NAME_REGEX: Pattern = re.compile(r".*?[^\s](?=<|\s+<)")
 
-HEADER_MAIL_REGEX: Pattern[str] = re.compile(
+HEADER_MAIL_REGEX: Pattern = re.compile(
     r"([^<>](?<=<)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?=>)|[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})"
 )
 
-NEW_LINE_REGEX: Pattern[str] = re.compile(r"\\R")
+NEW_LINE_REGEX: Pattern = re.compile(r"\\R")
 
-URL_REGEX: Pattern[str] = re.compile(r"(https?)(:\/\/[-_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+\$,%#]+)")
+URL_REGEX: Pattern = re.compile(r"(https?)(:\/\/[-_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+\$,%#]+)")
 
-HTML_TAG_REGEX: Pattern[str] = re.compile(r"<(\"[^\"]*\"|\'[^\']*\'|[^\'\">])*>")
+HTML_TAG_REGEX: Pattern = re.compile(r"<(\"[^\"]*\"|\'[^\']*\'|[^\'\">])*>")
 
-HTML_COMMENTS_REGEX: Pattern[str] = re.compile(r"<!--[\s\S]*?-->*")
+HTML_COMMENTS_REGEX: Pattern = re.compile(r"<!--[\s\S]*?-->*")
 
-HTML_STYLE_TAG_REGEX: Pattern[str] = re.compile(r"<style.*?>[\s\S]*<\/style>*")
+HTML_STYLE_TAG_REGEX: Pattern = re.compile(r"<style.*?>[\s\S]*<\/style>*")
 
-HTML_SCRIPT_TAG_REGEX: Pattern[str] = re.compile(r"<script.*?>[\s\S]*<\/script>*")
+HTML_SCRIPT_TAG_REGEX: Pattern = re.compile(r"<script.*?>[\s\S]*<\/script>*")
 
-MAIL_ADDRESS_REGEX: Pattern[str] = re.compile(
+MAIL_ADDRESS_REGEX: Pattern = re.compile(
     r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+"
 )
 
-TABS_REGEX: Pattern[str] = re.compile(r"\t+")
+TABS_REGEX: Pattern = re.compile(r"\t+")
 
-FULL_WITH_SPACE_REGEX: Pattern[str] = re.compile(r"　")
+FULL_WITH_SPACE_REGEX: Pattern = re.compile(r"　")
 
-UNICODE_FULL_WITH_SPACE_REGEX: Pattern[str] = re.compile(r"\u3000")
+UNICODE_FULL_WITH_SPACE_REGEX: Pattern = re.compile(r"\u3000")
 
 IDENTIFIER = re.compile(r"^[^\d\W]\w*\Z", re.UNICODE)
 
-SPACES_REGEX: Pattern[str] = re.compile(r"\s+")
+SPACES_REGEX: Pattern = re.compile(r"\s+")
 
 # List
 DEFAULT_COLUMNS: List[str] = [
