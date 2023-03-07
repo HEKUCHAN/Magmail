9992cf0aacf24c95fa55aeb09838b9c71d38e837
HEKUCHAN <heitorhirose@gmail.com>
2023-03-07 14:17:44 +0900

    一時的なcsv出力機能

--- src/magmail/magmail/magmail.py
+++ src/magmail/magmail/magmail.py
@@ -1,16 +1,19 @@
 import csv
+import math
 import email
 import mailbox
 from pathlib import Path
+from io import TextIOWrapper
 from mailbox import mboxMessage
 from email.message import Message
-from typing import Any, Iterator, Optional, Union, List, Dict, Callable
+from typing import Any, Generator, Iterator, Optional, Union, List, Dict, Callable
 
 from .filter import _Filter
 from magmail.mail import Mail
 from magmail.utils import to_Path
 from magmail.static import (
     DEFAULT_AUTO_CLEAN,
+    DEFAULT_COLUMNS,
     DEFAULT_CUSTOM_CLEAN_FUNCTIONS_DICT,
     DEFAULT_FILTER_CONTENTS_DICT,
     FILTER_CONTENTS_TYPE,
@@ -111,3 +114,63 @@ class Magmail:
                     with open(file, "rb") as email_file:
                         message = email.message_from_bytes(email_file.read())
                         self._append_mail(message, eml_path)
+
+    def split_emails(self, n: int) -> Generator[list[Mail], None, None]:
+        for idx in range(0, self.total(), n):
+            yield self.emails[idx:idx + n]
+
+    def export_csv(
+        self,
+        path: Union[str, Path] = "./mbox.csv",
+        filename: Optional[Union[str, Path]] = None,
+        encoding: str = "utf-8",
+        columns: List[str] = DEFAULT_COLUMNS.copy(),
+        extends_columns: List[str] = [],
+        slice_files: int = 1,
+    ) -> None:
+        """Export all mails of this class to csv
+
+        """
+        files: List[TextIOWrapper] = []
+        files_path: List[Path] = []
+        if extends_columns:
+            columns.extend(extends_columns)
+
+        csv_path =to_Path(path)
+
+        if slice_files > 1:
+            if csv_path.is_dir():
+                if filename is None:
+                    filename = Path("mbox.csv")
+
+                csv_filename = to_Path(filename)
+
+                for i in range(1, slice_files + 1):
+                    csv_filename = csv_filename.with_suffix(".csv")
+                    files_path.append(
+                        csv_path / (csv_filename.stem + f"-{i}" + csv_filename.suffix)
+                    )
+            else:
+                csv_filename = csv_path.with_suffix(".csv")
+                for i in range(1, slice_files + 1):
+                    files_path.append(
+                        csv_path.parent
+                        / (csv_filename.stem + f"-{i}" + csv_filename.suffix)
+                    )
+        else:
+            csv_path = csv_path.with_suffix(".csv")
+            files_path.append(csv_path)
+
+        for path in files_path:
+            files.append(open(path, "w", encoding=encoding, newline=""))
+
+        file_index = 0
+        for splitted_emails in self.split_emails(math.ceil(self.total() / slice_files)):
+            writer = csv.writer(files[file_index], quotechar='"')
+            writer.writerow(columns)
+            for mail in splitted_emails:
+                rows = []
+                for row in columns:
+                    rows.append(getattr(mail, row, None))
+                writer.writerow(rows)
+            file_index += 1
