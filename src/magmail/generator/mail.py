54935db32d2a03ab62a0bcee8ba60b45b74d3d63
HEKUCHAN <heitorhirose@gmail.com>
2023-04-30 01:35:26 +0900

    add encoding

--- src/magmail/generator/mail.py
+++ src/magmail/generator/mail.py
@@ -1,29 +1,66 @@
+from email.header import Header
 from email.mime.text import MIMEText
+from email.utils import formatdate, formataddr
 from email.mime.multipart import MIMEMultipart
+from typing import Dict, Tuple, Union
+
 
 class Mail:
     def __init__(
         self,
-        to,
-        h_from,
-        cc_mail,
-        subject,
-        message,
-        mime_type="plain"
+        to: Union[str, Tuple[str, str]],
+        h_from: Union[str, Tuple[str, str]],
+        cc_mail: Union[str, Tuple[str, str]],
+        subject: str,
+        message: Union[str, Dict[str, str]],
+        headers: Dict[str, str]={},
+        encoding: str="utf-8",
+        mime_type: str="plain",
     ):
         self.to = to
         self.h_from = h_from
         self.cc_mail = cc_mail
         self.subject = subject
         self.message = message
-        self.mime_type = mime_type
-        self.__init()
+        self.headers = headers
+        self.encoding = encoding
+        self.mime_type = mime_type.lower()
+        self.__body()
+        self.__headers()
 
-    def __init(self):
+    def __headers(self):
+        headers: Dict[str, Union[str, Tuple[str, str]]] = {
+            "Subject": self.to,
+            "From": self.h_from,
+            "To": self.cc_mail,
+            "Date": self.subject,
+        }.update(self.headers)
+
+        for name, header in headers.items():
+            if isinstance(header, tuple):
+
+                target_name = Header(header[0], self.encoding).encode()
+                email_addr = header[1]
+
+                self.mime[name] = formataddr((target_name, email_addr), charset=self.encoding)
+            else:
+                self.mime[name] = Header(header, self.encoding).encode()
+
+        self.mime["Date"] = formatdate()
+
+    def __body(self):
         if self.mime_type == "plain":
-            self.mime = MIMEText(self.message, 'plain')
+            self.mime = MIMEText(
+                self.message.encode(self.encoding),
+                "plain",
+                _charset=self.encoding
+            )
         elif self.mime_type == "html":
-            self.mime = MIMEText(self.message, 'html')
+            self.mime = MIMEText(
+                self.message.encode(self.encoding),
+                "html",
+                _charset=self.encoding
+            )
         elif self.mime_type == "multipart":
             self.mime = MIMEMultipart()
 
@@ -31,16 +68,13 @@ class Mail:
                 # TODO: 型が間違っているということを書く
                 raise TypeError()
 
-            if "plain" in self.message and "html" in self.message:
+            if not "plain" in self.message and not "html" in self.message:
                 # TODO: 辞書である必要があって、ある値がないといけないというエラーを記述
                 raise ""
 
-            self.mime.attach(MIMEText(self.message['plain'], 'plain'))
-            self.mime.attach(MIMEText(self.message['html'], 'html'))
-
-        self.mime["Subject"] = self.subject
-        self.mime["To"] = self.to
-        self.mime["From"] = self.h_from
-        self.mime["cc"] = self.cc_mail
-
-        
+            self.mime.attach(
+                MIMEText(self.message["plain"].encode(self.encoding), "plain", _charset=self.encoding)
+            )
+            self.mime.attach(
+                MIMEText(self.message["html"].encode(self.encoding), "html", _charset=self.encoding)
+            )
