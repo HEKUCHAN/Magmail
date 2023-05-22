c64138bf407e13abdd7acfea49d0b68668461449
HEKUCHAN <HEKUCHAN@users.noreply.github.com>
2023-05-22 03:57:21 +0000

    Apply Code Formatter Change

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
-def sample(a, aa, aaa, aaaa, aaaaa, aaaaaa, aaaaaaa, aaaaaaaa, aaaaaaaaa, aaaaaaaaaa, aaaaaaaaaaa,):
+def sample(
+    a,
+    aa,
+    aaa,
+    aaaa,
+    aaaaa,
+    aaaaaa,
+    aaaaaaa,
+    aaaaaaaa,
+    aaaaaaaaa,
+    aaaaaaaaaa,
+    aaaaaaaaaaa,
+):
     return
