445c3f35c53d2e5d904f68586f401266ff0f3d8a
HEKUCHAN <heitorhirose@gmail.com>
2023-06-22 02:26:00 +0900

    delete no need code

--- setup.py
+++ setup.py
@@ -17,19 +17,7 @@ setup(
     author="HEKUTA",
     author_email="Heitorhirose@gmail.com",
     url="https://github.com/HEKUCHAN/Magmail",
-    packages=find_packages(
-        "src",
-        exclude=[
-            'magmail/generator/seeds',
-            'magmail/generator/seeds_scripts',
-            'generator/seeds',
-            'generator/seeds_scripts',
-            'src/magmail/generator/seeds',
-            'src/magmail/generator/seeds_scripts',
-            'seeds',
-            'seeds_scripts',
-        ]
-    ),
+    packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=_requires_from_file("requirements.txt"),
     include_package_data=True,
