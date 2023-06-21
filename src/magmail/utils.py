23cf0c03f1776a984af64625b05a5a7715524efc
HEKUCHAN <heitorhirose@gmail.com>
2023-06-22 01:28:18 +0900

    add natural_keys func

--- src/magmail/utils.py
+++ src/magmail/utils.py
@@ -1,3 +1,4 @@
+import re
 import keyword
 from pathlib import Path
 from typing import Any, Optional, Union
@@ -29,3 +30,15 @@ def to_attribute_name(name: str) -> str:
         name = f"h_{name}"
 
     return name
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    https://stackoverflow.com/questions/5967500/how-to-correctly-sort-a-string-with-a-number-inside
+    '''
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
