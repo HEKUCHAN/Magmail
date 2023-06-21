0d167496e4263cf9ca548364f6186a5d2fe97ad1
HEKUCHAN <heitorhirose@gmail.com>
2023-06-22 01:30:21 +0900

    change faker var

--- src/magmail/generator/seeds_scripts/normal_jp.py
+++ src/magmail/generator/seeds_scripts/normal_jp.py
@@ -1,5 +1,6 @@
 import os
 import random
+from faker import Faker
 
 from magmail.generator import Seed, gen_email, gen_word, gen_name
 from magmail.types import HEADER_TYPE
@@ -11,10 +12,12 @@ generator = Seed(
     json_path="../seeds/normal_jp_seeds.json",
     export_eml_path="../../../../../tests/test_files/eml/normal_jp/",
     title="Normal Japanese Seeds",
-    explain="Normal Japanese mail samples",
+    explain="Normal Japanese mail samples. (encoding=UTF-8)",
 )
 
-LANG = "ja"
+faker = Faker("ja_JP")
+
+print("Start generate Japanese seeds...")
 
 for i in range(75):
     cc_range = 5
@@ -23,25 +26,26 @@ for i in range(75):
 
     if rand != (cc_range - 1):
         if i % 2 == 0:
-            cc_header_addr = [gen_email(LANG) for _ in range(rand)]
+            cc_header_addr = [faker.email() for _ in range(rand)]
         else:
-            cc_header_addr = [(gen_name(LANG), gen_email(LANG)) for _ in range(rand)]
+            cc_header_addr = [(faker.name(), faker.email()) for _ in range(rand)]
 
     if i % 2 == 0:
         generator.add(
-            addr_to=gen_email(LANG),
-            addr_from=gen_email(LANG),
+            addr_to=faker.email(),
+            addr_from=faker.email(),
             addr_cc=cc_header_addr,
-            subject=gen_word(length=25, lang=LANG),
-            message=gen_word(lang=LANG),
+            subject=faker.text(max_nb_chars=25),
+            message=faker.text(max_nb_chars=250),
         )
     else:
         generator.add(
-            addr_to=(gen_name(LANG), gen_email(LANG)),
-            addr_from=(gen_name(LANG), gen_email(LANG)),
+            addr_to=(faker.name(), faker.email()),
+            addr_from=(faker.name(), faker.email()),
             addr_cc=cc_header_addr,
-            subject=gen_word(length=25, lang=LANG),
-            message=gen_word(lang=LANG),
+            subject=faker.text(max_nb_chars=25),
+            message=faker.text(max_nb_chars=250),
         )
 
 generator.to_file(indent=2, encoding="utf-8")
+print("Generated Japanese seeds!")
